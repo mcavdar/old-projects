221e85820c636d102ff3ce3cf0c56c3a0eed36da
Mahmut CAVDAR <mahmutcvdr@gmail.com>
2014-04-24 16:48:35 +0300

    Update mat.cpp

--- mat.cpp
+++ mat.cpp
@@ -5,8 +5,11 @@
  *      Author: mc
  */
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
+
 int main(int argc, char** argv) {
 
 	Mat A;
@@ -42,12 +45,31 @@ int main(int argc, char** argv) {
 	/* Bu definition ile A'nın belirli
 	 * satır ve sütun aralığını B'e kopyalarız.*/
 
+	//Mat B(2,2,CV_8UC3,Scalar(255,255,255));
+	/* Bu definition ile B matrisi RGB değeri belirtilen
+	 * sayısı verilen pixellerden oluşur.*/
+
+	//Mat B(10,10,CV_8UC3,Scalar(0,0,255));
+
+	IplImage* img = cvLoadImage("image32.jpeg", 1);
+	Mat C(img);
+	/*Bu kod parçası ile resim IplImage türünde kaydedildikten sonra
+	 * nasıl mat türüne çevirileceğini görüyoruz.*/
+
+	Mat D = Mat(4, 4, CV_8UC3);
+	randu(D, Scalar::all(0), Scalar::all(255));
+	/* randu fonksiyonu değerler arasındaki herhangi rasgele sayılarla
+	 * matrisi doldurur.*/
+
+	cout << D << endl;
+
 	namedWindow("Image", WINDOW_NORMAL);
-	namedWindow("Image2", WINDOW_NORMAL);
-
-
+	//namedWindow("Image2", WINDOW_NORMAL);
+	namedWindow("Image3", WINDOW_NORMAL);
 	imshow("Image", A);
-	imshow("Image2", B);
+	//imshow("Image2", B);
+	imshow("Image3", D);
+
 	waitKey(0);
 
 	return 0;
