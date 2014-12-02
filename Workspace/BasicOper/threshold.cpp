a81ce64f7d967f58b59cfb6cee3efed3b65500c3
MC <mahmutcvdr@gmail.com>
2014-12-02 20:42:21 +0200

    all threshold example (binary,inv,truncate,thrtozero..)

--- Workspace/BasicOper/threshold.cpp
+++ Workspace/BasicOper/threshold.cpp
@@ -11,12 +11,16 @@ using namespace std;
 
 /** @function main */
 int main(int argc, char** argv) {
-	Mat src,dst;
+	Mat src, dst;
 
 	src = imread("2.jpg");
 
-	cvtColor(src,src,CV_BGR2GRAY);
-	threshold(src,dst,100,255,0);
+	cvtColor(src, src, CV_BGR2GRAY);
+	threshold(src, dst, 100, 255, 0); // 100den büyük değerleri 255 yapar kalanını 0 yapar. Binary
+	threshold(src, dst, 100, 255, 1); // 100den büyük değerleri 0 yapar diğerlerini 255 yapar. Binary Inverted
+	threshold(src, dst, 100, 0, 2); // 100den büyük değerleri 100'e yuvarlar. Truncate
+	threshold(src, dst, 70, 0, 3); // 100den küçük değerleri 0'a yuvarlar. Threshold to zero
+	threshold(src, dst, 100, 0, 4); // 100den büyük değerleri 0'a yuvarlar. Threshold to zero inverted
 
 	//std::cout << "M = " << std::endl << " " << dst << std::endl << std::endl;
 	namedWindow("mc", CV_WINDOW_AUTOSIZE);
@@ -26,10 +30,4 @@ int main(int argc, char** argv) {
 
 	return 0;
 }
-
-
-
-
-
-
 
